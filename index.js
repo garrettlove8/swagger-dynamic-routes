7b17c48471ec00bc2c32343d2d41e9a7f34b6c0a
gxl054 <gxl054@jefferson.edu>
2020-09-02 10:48:51 -0400

    plan

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 /*
   TODO: To-do list:
-  1. generate json
-  2. take in data
+  x1. generate json
+  x2. take in data
   4. figure out the structure of how we need the data to look
   5. Loop through data and create Swagger json according to the swagger spec
+*/
+
+/*
+  - Array of options for each parameter in the route
+  - Object for each route combo with properties
+      - crud: [c, r, u, d]
+      - request (corresponding to array indexes of crud prop)
+      - response (corresponding to array indexes of crud prop)
 */
 
 const fs = require('fs')
